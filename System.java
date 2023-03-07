992fe687821ba1fd9c3e1dbc0135d9c0554d1ff7
Mary Nguyen <maryn77@my.yorku.ca>
2023-03-06 23:53:10 -0500

    update

--- System.java
+++ System.java
@@ -1,8 +1,18 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class System {
     public System() {
         JFrame frame = new JFrame();
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+
+        JButton button = new JButton();
+        frame.getContentPane().add(button);
+    }
+
+    public static void main(String args[]) {
+        new System();
     }
 }
 
