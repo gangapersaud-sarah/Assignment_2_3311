28c0ae683073438386585367d2af3cb01278a59c
Mary Nguyen <maryn77@my.yorku.ca>
2023-03-12 22:12:02 -0400

    Change to Jframe for ActionListener to work

--- AddBooking.java
+++ AddBooking.java
@@ -1,29 +1,20 @@
-
+import java.awt.Color;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.Font;
-import java.awt.TextField;
 import java.awt.Label;
-import java.awt.ScrollPane;
-import java.awt.Scrollbar;
-import java.awt.Choice;
-import javax.swing.JMenuBar;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
-import java.awt.Color;
-import javax.swing.JComboBox;
+public class AddBooking extends JFrame {
 
-public class AddBooking {
-
-	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -32,8 +23,8 @@ public class AddBooking {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					AddBooking window = new AddBooking();
-					window.frame.setVisible(true);
+					AddBooking frame = new AddBooking();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -42,71 +33,67 @@ public class AddBooking {
 	}
 
 	/**
-	 * Create the application.
+	 * Create the frame.
 	 */
 	public AddBooking() {
-		initialize();
-	}
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 800, 600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Add Booking");
 		lblNewLabel.setBackground(new Color(192, 192, 192));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 56));
 		lblNewLabel.setBounds(0, 0, 786, 124);
-		frame.getContentPane().add(lblNewLabel);
+		contentPane.add(lblNewLabel);
 		
 		
 		Label label = new Label("License Plate:");
 		label.setFont(new Font("Tahoma", Font.BOLD, 20));
 		label.setBounds(52, 178, 216, 48);
-		frame.getContentPane().add(label);
+		contentPane.add(label);
 		
 		Label label_1 = new Label("Date:");
 		label_1.setFont(new Font("Tahoma", Font.BOLD, 20));
 		label_1.setBounds(52, 247, 199, 48);
-		frame.getContentPane().add(label_1);
+		contentPane.add(label_1);
 		
 		Label label_2 = new Label("Time:");
 		label_2.setFont(new Font("Tahoma", Font.BOLD, 20));
 		label_2.setBounds(52, 316, 199, 48);
-		frame.getContentPane().add(label_2);
+		contentPane.add(label_2);
 		
 		Label label_2_1 = new Label("Duration:");
 		label_2_1.setFont(new Font("Tahoma", Font.BOLD, 20));
 		label_2_1.setBounds(52, 391, 199, 48);
-		frame.getContentPane().add(label_2_1);
+		contentPane.add(label_2_1);
 		
-		textField = new JTextField();
+		JTextField textField = new JTextField();
 		textField.setBounds(334, 178, 316, 48);
-		frame.getContentPane().add(textField);
+		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		JButton btnNewButton = new JButton("Add Booking");
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 30));
 		btnNewButton.setBounds(260, 450, 257, 88);
-		frame.getContentPane().add(btnNewButton);
+		contentPane.add(btnNewButton);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(334, 316, 316, 48);
-		frame.getContentPane().add(comboBox);
+		contentPane.add(comboBox);
 		
 		JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setBounds(334, 391, 316, 48);
-		frame.getContentPane().add(comboBox_1);
+		contentPane.add(comboBox_1);
 		
-		textField_1 = new JTextField();
+		JTextField textField_1 = new JTextField();
 		textField_1.setBounds(334, 247, 316, 48);
-		frame.getContentPane().add(textField_1);
+		contentPane.add(textField_1);
 		textField_1.setColumns(10);
-		
 	}
+
 }
