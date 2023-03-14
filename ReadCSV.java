2c433767c512cbc57a8f3c2880d24e98ef56c815
Mary Nguyen <maryn77@my.yorku.ca>
2023-03-13 22:05:32 -0400

    update Read CSV, Add ViewBooking

--- ReadCSV.java
+++ ReadCSV.java
@@ -11,20 +11,24 @@ public class ReadCSV {
 	// testing delete later
 	public static void main(String[]args) {
 		String filepath = "Clients.txt";
-		String username = "HappyBuddy99";
+		String username = "HappyBuddy88";
+		String type = "Manager";
 		
-		findUser(username, filepath);
+		findUserName(username, filepath);
+		findUserType(type, filepath);
+		//allUsers(filepath);
 	}
+
 	
-	// reading the client CSV
 	/**
+	 * reading the client CSV
 	 * @param findUsername = username (unique) of the user we want to find
 	 * @param filepath = Clients.txt
 	 * @return the string array containing {username, firstname, lastname, email, password, type}
 	 */
-	public static ArrayList<String> findUser(String findUsername, String filepath) {
-		boolean found = false;
+	public static ArrayList<String> findUserName(String findUsername, String filepath) {
 		
+		boolean found = false;
 		ArrayList<String> returnList = new ArrayList<String>();
 		String username = "";
 		String first = "";
@@ -38,7 +42,7 @@ public class ReadCSV {
 			// create reading tools, x is the file
 			x = new Scanner(new File(filepath));
 			// Separate values in x using "," and "\n"
-			x.useDelimiter("[,\n]");
+			x.useDelimiter("[,\n\r]");
 			
 			while (x.hasNext() && !found) {
 				
@@ -67,6 +71,233 @@ public class ReadCSV {
 			if(found) {
 				JOptionPane.showMessageDialog(null, username + ", " + first + ", " + last + ", " + email + ", " + pwd + ", " + type);
 			}
+		}
+		catch (Exception e){
+			
+		}
+		return returnList;
+	}
+	
+	/**
+	 * reading the client CSV
+	 * @param findUsername = username (unique) of the user we want to find
+	 * @param filepath = Clients.txt
+	 * @return the string array containing {username, firstname, lastname, email, password, type}
+	 */
+	public static ArrayList<ArrayList<String>> findUserType(String findtype, String filepath) {
+		
+		ArrayList<ArrayList<String>> returnList = new ArrayList<ArrayList<String>>();
+		String username = "";
+		String first = "";
+		String last = "";
+		String email = "";
+		String pwd = "";
+		String type = "";
+		
+		try {
+			
+			// create reading tools, x is the file
+			x = new Scanner(new File(filepath));
+			// Separate values in x using "," and "\n"
+			x.useDelimiter("[,\n]");
+			
+			while (x.hasNext()) {
+				
+				// get next line
+				username = x.next();
+				first = x.next();
+				last = x.next();
+				email = x.next();
+				pwd = x.next();
+				type = x.next();
+				
+				// when user is found (unique usernames)
+				if(type.equals(findtype)) {
+					ArrayList<String> list = new ArrayList<String>();
+					// add the user details to the array
+					list.add(username);
+					list.add(first);
+					list.add(last);
+					list.add(email);
+					list.add(pwd);
+					list.add(type);
+					
+					returnList.add(list);
+					
+					JOptionPane.showMessageDialog(null, username + ", " + first + ", " + last + ", " + email + ", " + pwd + ", " + type);
+
+				}
+			}
+			
+		}
+		catch (Exception e){
+			
+		}
+		return returnList;
+	}
+	
+		/**
+		 * reading the Booking CSV
+		 * @param findUsername = username (unique) of the user we want to find
+		 * @param filepath = Bookings.txt
+		 * @return the string array containing {username, license plate, parking lot, parking space, date, time, duration}
+		 */
+		public static ArrayList<String> findBooking(String findUsername, String filepath) {
+			
+			boolean found = false;
+			ArrayList<String> returnList = new ArrayList<String>();
+			String username = "";
+			String lp = "";
+			String pl = "";
+			String ps = "";
+			String date = "";
+			String time = "";
+			String duration = "";
+			
+			try {
+				
+				// create reading tools, x is the file
+				x = new Scanner(new File(filepath));
+				// Separate values in x using "," and "\n"
+				x.useDelimiter("[,\n]");
+				
+				while (x.hasNext() && !found) {
+					
+					// get next line
+					username = x.next();
+					lp = x.next();
+					pl = x.next();
+					ps = x.next();
+					date = x.next();
+					time = x.next();
+					duration = x.next();
+					
+					// when booking is found (unique usernames)
+					if(username.equals(findUsername)) {
+						found = true;
+						// add the booking details to the array
+						returnList.add(username);
+						returnList.add(lp);
+						returnList.add(pl);
+						returnList.add(ps);
+						returnList.add(date);
+						returnList.add(time);
+						returnList.add(duration);
+						
+						
+					}
+				}
+				
+				if(found) {
+					JOptionPane.showMessageDialog(null, username + ", " + lp + ", " + pl + ", " + ps + ", " + date + ", " + time + ", " + duration);
+				}
+			}
+			catch (Exception e){
+				
+			}
+			return returnList;
+		}
+	
+	/**
+	 * return all users of the client CSV
+	 * @param filepath
+	 * @return an Array list of array lists, every user
+	 */
+	public static ArrayList<ArrayList<String>> allUsers(String filepath) {
+	
+		ArrayList<ArrayList<String>> returnList = new ArrayList<ArrayList<String>>();
+		String username = "";
+		String first = "";
+		String last = "";
+		String email = "";
+		String pwd = "";
+		String type = "";
+		
+		try {
+			
+			// create reading tools, x is the file
+			x = new Scanner(new File(filepath));
+			// Separate values in x using "," and "\n"
+			x.useDelimiter("[,\n]");
+			
+			while (x.hasNext()) {
+				
+				// get next line
+				username = x.next();
+				first = x.next();
+				last = x.next();
+				email = x.next();
+				pwd = x.next();
+				type = x.next();
+				
+				ArrayList<String> list = new ArrayList<String>();
+				// add the user details to the array
+				list.add(username);
+				list.add(first);
+				list.add(last);
+				list.add(email);
+				list.add(pwd);
+				list.add(type);
+				
+				// add the user (array list of details) to the arraylist of users
+				returnList.add(list);
+				
+			}
+			
+		}
+		catch (Exception e){
+			
+		}
+		return returnList;
+	}
+	
+	
+	
+public static ArrayList<ArrayList<String>> allBookings(String filepath) {
+	
+		
+		ArrayList<ArrayList<String>> returnList = new ArrayList<ArrayList<String>>();
+		String username = "";
+		String lp = "";
+		String pl = "";
+		String ps = "";
+		String date = "";
+		String time = "";
+		String duration = "";
+		
+		try {
+			
+			// create reading tools, x is the file
+			x = new Scanner(new File(filepath));
+			// Separate values in x using "," and "\n"
+			x.useDelimiter("[,\n]");
+			
+			while (x.hasNext()) {
+				
+				// get next line
+				username = x.next();
+				lp = x.next();
+				pl = x.next();
+				ps = x.next();
+				date = x.next();
+				time = x.next();
+				duration = x.next();
+				
+				ArrayList<String> list = new ArrayList<String>();
+				// add the user details to the array
+				list.add(username);
+				list.add(lp);
+				list.add(pl);
+				list.add(ps);
+				list.add(date);
+				list.add(time);
+				list.add(duration);
+				
+				// add the booking (array list of details) to the arraylist of bookings
+				returnList.add(list);
+				
+			}
+			
 		}
 		catch (Exception e){
 			
