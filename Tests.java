32af8c9bf1f05391bee4c97f91ff472bc4361931
M0hamm3d 8a19 <mohammedR2244@gmail.com>
2023-04-13 21:51:39 -0400

    Update test

--- Tests.java
+++ Tests.java
@@ -113,5 +113,112 @@ public class Tests {
 
 	}
 
+	@SuppressWarnings("deprecation")
+	@Test
+	public void test1() {
+		
+		//manager medium 
+		manager_medium m = manager_medium.getInstance();
+		boolean b1 = m.equals(null);
+		assertEquals(false,b1);
+		
+		//super manager main page
+		superManagerMainPage sm = superManagerMainPage.getInstance();
+		boolean b2 = sm.equals(null);
+		assertEquals(false,b2);
+		
+		//create auto account test
+		createAutoAccount s = new createAutoAccount();
+		WriteCSV.CreateAutoAccounts("test_user1", "test_pass1");
+		ArrayList<String> returnList = new ArrayList<String>();
+		ArrayList<String> assertionList = new ArrayList<String>();
+		returnList = ReadCSV.findUserName("test_user1", "Clients.txt");
+		assertionList.add("test_user1");
+		assertionList.add("null");
+		assertionList.add("null");
+		assertionList.add("null");
+		assertionList.add("test_pass1");
+		assertionList.add("Manager");
+		assertionList.add("-1\r");
+		assertEquals(assertionList,returnList);
+		
+		//view auto account test
+		viewAutoAccount v = new viewAutoAccount();
+		//number of rows(at the time of this running there should only be 7, 6 in clients.txt and one made prior)
+		int numberRows = v.numberRows();
+		assertEquals(7,numberRows);
+		
+		//number of columns(this remains consistent)
+		int numberColumns = v.numberColumns();
+		assertEquals(7,numberColumns);
+	    
+		//elements in list
+		Object[] row = new Object[7];
+		row = v.getAutoAccount(0);
+		String[] test1 = {"HappyBuddy11","null","null","null","7890","Manager","-1"};
+		assertEquals(row[0],test1[0]);
+		assertEquals(row[1],test1[1]);
+		assertEquals(row[2],test1[2]);
+		assertEquals(row[3],test1[3]);
+		assertEquals(row[4],test1[4]);
+		assertEquals(row[5],test1[5]);
+		
+		row = v.getAutoAccount(1);
+		String[] test11 = {"e2xb4qma4q","null","null","null","NZ#86l6lDX","Manager","-1"};
+		assertEquals(row[0],test11[0]);
+		assertEquals(row[1],test11[1]);
+		assertEquals(row[2],test11[2]);
+		assertEquals(row[3],test11[3]);
+		assertEquals(row[4],test11[4]);
+		assertEquals(row[5],test11[5]);
+		
+		row = v.getAutoAccount(2);
+		String[] test111 = {"ps3ncq640g","null","null","null","z^UA8L1%Nj","Manager","-1"};
+		assertEquals(row[0],test111[0]);
+		assertEquals(row[1],test111[1]);
+		assertEquals(row[2],test111[2]);
+		assertEquals(row[3],test111[3]);
+		assertEquals(row[4],test111[4]);
+		assertEquals(row[5],test111[5]);
+		
+		row = v.getAutoAccount(3);
+		String[] test1111 = {"HappyBuddy"," null"," null"," null"," 1234","Manager","-1"};
+		assertEquals(row[0],test1111[0]);
+		assertEquals(row[1],test1111[1]);
+		assertEquals(row[2],test1111[2]);
+		assertEquals(row[3],test1111[3]);
+		assertEquals(row[4],test1111[4]);
+		assertEquals(row[5],test1111[5]);
+		
+		row = v.getAutoAccount(4);
+		String[] test11111 = {"6fbl67p19h"," null"," null"," null"," wKr9J^y%7B","Manager","-1"};
+		assertEquals(row[0],test11111[0]);
+		assertEquals(row[1],test11111[1]);
+		assertEquals(row[2],test11111[2]);
+		assertEquals(row[3],test11111[3]);
+		assertEquals(row[4],test11111[4]);
+		assertEquals(row[5],test11111[5]);
+		
+		row = v.getAutoAccount(5);
+		String[] test111111 = {"z4s97ff1e6","null","null","null","jeE7!KBGVo","Manager","-1"};
+		assertEquals(row[0],test111111[0]);
+		assertEquals(row[1],test111111[1]);
+		assertEquals(row[2],test111111[2]);
+		assertEquals(row[3],test111111[3]);
+		assertEquals(row[4],test111111[4]);
+		assertEquals(row[5],test111111[5]);
+		
+		row = v.getAutoAccount(6);
+		String[] test1111111 = {"test_user1","null","null","null","test_pass1","Manager","-1"};
+		assertEquals(row[0],test1111111[0]);
+		assertEquals(row[1],test1111111[1]);
+		assertEquals(row[2],test1111111[2]);
+		assertEquals(row[3],test1111111[3]);
+		assertEquals(row[4],test1111111[4]);
+		assertEquals(row[5],test1111111[5]);
+		
+		
+	}
+
 }
 
